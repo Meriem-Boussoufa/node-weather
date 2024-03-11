cda231e0a62c62a6e5d88807c9f2575bc6890182
meriem <meriem@gmail.com>
2024-03-11 10:48:22 +0100

    Customize HTTP Requests

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
 const url2 = 'https://api.openweathermap.org/data/3.0/onecall?lat=51.507&lon=-0.12&appid=8577e30411acb9ecc027a7a3f79a66ef'
-request({url: url2}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+
+request({url: url2, json: true}, (error, response) => {
+    console.log('It is current: ' + response.body.current.temp + ' degrees out. It feels like ' + response.body.current.feels_like + ' degress out.')
 })
 
