860c9364ba4382076b6c72247b0822da3ac7ef02
meriem <meriem@gmail.com>
2024-03-11 11:03:44 +0100

    http request challenge

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ request({url: url2, json: true}, (error, response) => {
     console.log('It is current: ' + response.body.current.temp + ' degrees out. It feels like ' + response.body.current.feels_like + ' degress out.')
 })
 
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWVyaWVtLWJvdXNzb3VmYSIsImEiOiJjbHRtcnZzd3AxZXQ0MnFuemEyN252YTNpIn0._sWECSNbYXdHBiQ--Uxmrg&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
