2347c4f515e464ffcba4ce3e44c2b60c10ab0837
meriem <meriem@gmail.com>
2024-03-11 12:26:33 +0100

    property shorthand

--- app.js
+++ app.js
@@ -42,18 +42,20 @@ const address = process.argv[2]
 
 if(!address) {
     console.log('Please provide and address !')
-}
-
-geocode(address, (error, data) => {
-    if(error) {
-        return  console.log('Error', error)
-    }
-    forecast(data.latitude, data.longitude, (error,forecastData) => {
+}else{
+    geocode(address, (error, {latitude,longitude,location}) => {
         if(error) {
             return  console.log('Error', error)
         }
-        console.log(data.location)
-        console.log(forecastData)
+        forecast(latitude, longitude, (error,forecastData) => {
+            if(error) {
+                return  console.log('Error', error)
+            }
+            console.log(location)
+            console.log(forecastData)
+        })
     })
-})
+}
+
+
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast =(latitude, longtitude, callback) => {
 
 const url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longtitude + "&appid=8577e30411acb9ecc027a7a3f79a66ef"
-    request({ url : url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service !', undefined)
-        }else if(response.body.error) {
+        }else if(body.error) {
             callback('Unable to find location !', undefined)
         }else {
-            callback(undefined, response.body.current.weather[0].main + ' It is current: ' + response.body.current.temp + ' degrees out. It feels like ' + response.body.current.feels_like + ' degress out.')
+            callback(undefined, body.current.weather[0].main + ' It is current: ' + body.current.temp + ' degrees out. It feels like ' + body.current.feels_like + ' degress out.')
         }
     })
 }
