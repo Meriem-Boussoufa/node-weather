8ce2a8e79bdb37b9ec78f28de99cb0b9cd601625
meriem <meriem@gmail.com>
2024-03-12 13:26:45 +0100

    wiring up the user interface

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,20 +47,6 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia',
-        address: req.query.address
-    })
-})
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-
     geocode(req.query.address, (error, { latitude, longitude, location }) => {
         if (error) {
             return res.send({ error })
