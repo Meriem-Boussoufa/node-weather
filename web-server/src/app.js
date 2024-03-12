d68b318f5076900f2dd91c1f89ec8dfb0e7dd58a
meriem <meriem@gmail.com>
2024-03-12 12:08:10 +0100

    dynamic pages with template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,33 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Meriem'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Meriem'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        name: 'Meriem'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
